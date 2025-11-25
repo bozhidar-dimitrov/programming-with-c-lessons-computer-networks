49f0f96b39c91a41a59d993d901f8b12bf8e2b6e
Bozhidar Dimitrov <bozhidar.dimitrov@gmail.com>
2025-11-25 10:53:45 +0200

    Debugging

--- 4-programming-environment/debugging.c
+++ 4-programming-environment/debugging.c
@@ -3,7 +3,7 @@
 void bubbleSort(int arr[], int size) {
   for (int i = 0; i < size - 1; i++){
     for (int j = 0; j < size - 1 - i; j++){
-      if (arr[j] < arr[j+1]) {
+      if (arr[j] > arr[j+1]) {
         int temp = arr[j];
         arr[j] = arr[j+1];
         arr[j+1] = temp;
